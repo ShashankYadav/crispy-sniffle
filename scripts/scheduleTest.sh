44281a1f6806e4877d2c28aa45288dc2071c1671
syadav <syadav@arista.com>
2017-11-20 15:55:42 +0530

    fix path

--- scripts/scheduleTest.sh
+++ scripts/scheduleTest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Running command on us155"
-ssh -F /home/arastra/.ssh/config arastra@us155.sjc -C "a4 mut status -m syadav.fix-autosubmitted-emails.0"
+ssh -F /var/lib/jenkins/config arastra@us155.sjc -C "a4 mut status -m syadav.fix-autosubmitted-emails.0"
